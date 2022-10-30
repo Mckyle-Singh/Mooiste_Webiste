7f82d78d1b2f1c4b23bdd60667afc54d4f18616c
Denzel Witbooi <dwitbooi28@gmail.com>
2022-10-31 01:31:20 +0200

    Client side done- testing needed

--- BlazorMooiste/Client/Program.cs
+++ BlazorMooiste/Client/Program.cs
@@ -1,11 +1,22 @@
+global using BlazorMooiste.Shared;
+global using System.Net.Http.Json;
+global using BlazorMooiste.Client.Services.ProductService;
+
 using BlazorMooiste.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Blazored.LocalStorage;
+
+using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IProductService, ProductService>();
+
 
 await builder.Build().RunAsync();
