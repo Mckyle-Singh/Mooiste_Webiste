cfca762c4c952c46824df2d573cb41279481a59c
Denzel Witbooi <dwitbooi28@gmail.com>
2022-10-30 16:10:04 +0200

    seed data

--- BlazorMooiste/Server/Migrations/20221030135336_ProductSeeding.Designer.cs
+++ BlazorMooiste/Server/Migrations/20221030135336_ProductSeeding.Designer.cs
@@ -3,6 +3,7 @@ using BlazorMooiste.Server.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BlazorMooiste.Server.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20221030135336_ProductSeeding")]
+    partial class ProductSeeding
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
