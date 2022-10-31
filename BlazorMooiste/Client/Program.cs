78bdd9cb67fcb2a1f50b31b303ac50d583a76be9
Denzel Witbooi <dwitbooi28@gmail.com>
2022-10-31 17:11:09 +0200

    Admin page

--- BlazorMooiste/Client/Program.cs
+++ BlazorMooiste/Client/Program.cs
@@ -1,13 +1,27 @@
 global using BlazorMooiste.Shared;
 global using System.Net.Http.Json;
 global using BlazorMooiste.Client.Services.ProductService;
-
+global using BlazorMooiste.Client.Services.CategoryService;
+global using BlazorMooiste.Client.Services.AuthService;
+global using Microsoft.AspNetCore.Components.Authorization;
+global using BlazorMooiste.Client.Services.CartService;
+global using BlazorMooiste.Client.Services.OrderService;
+global using BlazorMooiste.Client.Services.AddressService;
+global using BlazorMooiste.Client.Services.ProductTypeService;
 using BlazorMooiste.Client;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Blazored.LocalStorage;
-
+using BlazorMooiste.Client.Services.ProductTypeService;
 using MudBlazor.Services;
+using BlazorMooiste.Client.Services.AddressService;
+using BlazorMooiste.Client.Services.AuthService;
+using BlazorMooiste.Client.Services.CartService;
+using BlazorMooiste.Client.Services.CategoryService;
+using BlazorMooiste.Client.Services.OrderService;
+using BlazorMooiste.Client.Services.ProductService;
+using BlazorMooiste.Client.Services.ProductTypeService;
+using BlazorMooiste.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -17,6 +31,14 @@ builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddMudServices();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
-
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<ICartService, CartService>();
+builder.Services.AddScoped<IAuthService, AuthService>();
+builder.Services.AddScoped<IOrderService, OrderService>();
+builder.Services.AddScoped<IAddressService, AddressService>();
+builder.Services.AddScoped<IProductTypeService, ProductTypeService>();
+builder.Services.AddOptions();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthStateProvider>();
 
 await builder.Build().RunAsync();
